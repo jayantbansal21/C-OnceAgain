43304111496de385ceee1c59b6585e31959c43ca
JAYANT <bansal.jayant8737@gmail.com>
2021-05-07 16:53:49 +0530

    semifinal

---
+++ bubblesort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void bubble(int arr[], int n)
+{
+    int temp;
+    for (int j = n-1; j >=0; j--)
+    {
+        for (int i; i <=j; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 5, 2, 9, 2, 4, 6};
+    bubble(arr, 7);
+    for (int i = 0; i < 7; i++)
+    {
+        cout<< arr[i] << " " <<endl;
+    }
+}
