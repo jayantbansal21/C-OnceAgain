eec3c7ff98aba602907ad8d52345fefd598ba38c
JAYANT <bansal.jayant8737@gmail.com>
2021-05-08 10:24:37 +0530

    semifinal

---
+++ DNFSort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+void swap(int arr[],int a,int b){
+    int temp;
+    temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+void dnf(int arr[],int n){
+    int low=0;
+    int mid=0;
+    int high=n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr,low,mid);
+            low++;mid++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+        else {
+            swap(arr,mid,high);
+            high--;
+        }
+
+    }
+}
+
+int main(){
+
+    int arr[]={1,0,2,0,1,1,2,0,2};
+    dnf(arr,9);
+
+    for(int i=0;i<9;i++){
+        cout<<arr[i];
+    }
+}
