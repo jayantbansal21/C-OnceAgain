e50d9b0225adaa09cc9fa10bd9904f975c6a88f2
JAYANT <bansal.jayant8737@gmail.com>
2021-05-10 19:36:59 +0530

    Deletion func added

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -56,12 +56,27 @@ void display(node *head)
     }
 }
 
+void deletehead(node *head){
+
+}
+void deletion(node *head,int val){
+    node* temp = head;
+    while(temp->next->data!=val){
+        temp=temp->next;
+    }
+    node* todelete = temp->next;  //This because after the next line node will be deleted but we need to deallocate it too , so this will have the address
+    temp->next=temp->next->next;
+    delete todelete;
+}
 int main()
 {
     node *head = NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
+    insertAtTail(head,3);
     display(head);
-    cout << search(head, 2);
+    cout << search(head, 2)<<endl;
+    deletion(head,3);
+    display(head);
     return 0;
 }
